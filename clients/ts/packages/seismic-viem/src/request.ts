a2f4f68dd90bfa02542d5c44b16b7d2dd20ffbdf
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2026-04-15 15:32:52 -0400

    delete unused request.ts file

--- clients/ts/packages/seismic-viem/src/request.ts
+++
@@ -1,6 +0,0 @@
-import { ExactPartial } from 'viem'
-import type { Chain, SendTransactionParameters } from 'viem'
-
-export type AssertSeismicRequestParameters = ExactPartial<
-  SendTransactionParameters<Chain>
->
