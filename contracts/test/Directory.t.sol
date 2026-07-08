01f183ca810bf65ea26c411d83905d0e0c1b4dd2
Ameya Deshmukh <ad@seismic.systems>
2026-07-08 21:30:43 +0530

    test(std-lib): cover checkHasKey, packEncryptedData, generateRandomNonce, HKDFDeriveKey

--- contracts/test/Directory.t.sol
+++ contracts/test/Directory.t.sol
@@ -30,6 +30,39 @@ contract DirectoryTest is Test {
         assertEq(decryptResult, sampleMsg);
     }
 
+    function test_CheckHasKeyReturnsFalseForUnsetAddress() public {
+        address carol = makeAddr("carol");
+        assertFalse(directory.checkHasKey(carol));
+    }
+
+    function test_CheckHasKeyReturnsTrueAfterSetKey() public {
+        address carol = makeAddr("carol");
+        vm.prank(carol);
+        directory.setKey(suint256(0x123));
+
+        assertTrue(directory.checkHasKey(carol));
+    }
+
+    function test_PackEncryptedDataAppendsTwelveByteNonce() public view {
+        bytes memory ciphertext = hex"deadbeefcafe";
+        uint96 nonce = 0x0102030405060708090a0b0c;
+
+        bytes memory packed = directory.packEncryptedData(ciphertext, nonce);
+
+        assertEq(packed.length, ciphertext.length + 12);
+    }
+
+    function test_PackEncryptedDataRoundTripsThroughParse() public view {
+        bytes memory ciphertext = hex"00112233445566778899aabbccddeeff0011223344";
+        uint96 nonce = type(uint96).max - 7;
+
+        bytes memory packed = directory.packEncryptedData(ciphertext, nonce);
+        (bytes memory parsedCiphertext, uint96 parsedNonce) = directory.parseEncryptedData(packed);
+
+        assertEq(parsedCiphertext, ciphertext);
+        assertEq(parsedNonce, nonce);
+    }
+
     function testEncryptSequence() public {
         bytes memory encryptedDataBob = directory.encrypt(bob, sampleMsg);
         bytes memory encryptedDataAlice = directory.encrypt(alice, sampleMsg);
